2873875e3ec060284147b4f7c5acaf2926988446
white0221 <whiterm.r0221@gmail.com>
2018-11-02 23:57:35 +0900

    c1 input button

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello, Vue.js',
+        message: '初期メッセージ',
         list: ['りんご', 'ばなな', 'いちご']
+    },
+    methods: {
+        handleClick: function(event) {
+            alert(event.target)
+        }
     }
 })
